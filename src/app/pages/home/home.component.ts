f36ce5dffd084965f4d3302c6eba5aa6ed1ded3b
Ivan <ielizalde.dev@gmail.com>
2024-10-10 23:44:34 +0200

    add header with the logo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CharacterListComponent } from "../../components/character-list/character-list.component";
+import { HeaderComponent } from "../../components/header/header.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CharacterListComponent],
+  imports: [CharacterListComponent, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
